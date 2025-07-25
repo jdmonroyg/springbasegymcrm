9788d964f742db37c2b5fef7472d3a70161c0e60
Jesús Monroy <jdmonroyg@unal.edu.co>
2025-07-25 18:43:58 -0500

    chore(init): initial project creation with readme and gitignore

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Spring Base - Gym CRM System
+
+This project is a CRM for managing a gym, using traditional Spring Core.
+
+## Stack
+
+- Java 21
+- Maven
+- Spring Core
+
+## Prerequisites
+
+- Java 21 SDK
+- Apache Maven 3.6 or Higher
+- Intellij IDEA
+- Git
---
+++ src/main/java/com/epam/Main.java
@@ -0,0 +1,13 @@
+package com.epam;
+
+/**
+ * @author jdmon on 25/07/2025
+ * @project Default (Template) Project
+ */
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
