ef86cbfd036454a26fde4bc066aa10d7a46b5d84
Jesús Monroy <jdmonroyg@unal.edu.co>
2025-08-20 19:16:18 -0500

    refactor(facade): Refactoring facade with new methods for the gym crm

--- src/main/java/com/epam/facade/PortalGymFacade.java
+++ src/main/java/com/epam/facade/PortalGymFacade.java
@@ -3,145 +3,195 @@ package com.epam.facade;
 import com.epam.model.Trainee;
 import com.epam.model.Trainer;
 import com.epam.model.Training;
+import com.epam.service.AuthService;
 import com.epam.service.TraineeService;
 import com.epam.service.TrainerService;
 import com.epam.service.TrainingService;
+import jakarta.validation.constraints.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
 
 import java.time.LocalDate;
 import java.util.List;
+
+import static com.epam.util.Constants.*;
 
 /**
  * @author jdmon on 27/07/2025
  * @project springbasegymcrm
  */
 @Service
+@Validated
 public class PortalGymFacade {
 
     private final TraineeService traineeService;
     private final TrainerService trainerService;
     private final TrainingService trainingService;
+    private final AuthService authService;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PortalGymFacade.class);
 
-    public PortalGymFacade(TraineeService traineeService,
-                           TrainerService trainerService, TrainingService trainingService) {
+    public PortalGymFacade(TraineeService traineeService, TrainerService trainerService, TrainingService trainingService, AuthService authService) {
         this.traineeService = traineeService;
         this.trainerService = trainerService;
         this.trainingService = trainingService;
+        this.authService = authService;
     }
 
-    public void createTrainee(String firstName, String lastName,
+    public void createTrainer(@NotBlank (message = INVALID_FIELD)
+                              String firstName,
+                              @NotBlank (message = INVALID_FIELD)
+                              String lastName,
+                              @Positive (message = INVALID_ID_VALUE)
+                              Long trainingTypeId){
+        LOGGER.info("Creating a trainer");
+        trainerService.createTrainer(firstName, lastName, trainingTypeId);
+        LOGGER.info("A trainer was created");
+    }
+
+    public void createTrainee(@NotBlank (message = INVALID_FIELD)
+                              String firstName,
+                              @NotBlank (message = INVALID_FIELD)
+                              String lastName,
                               LocalDate dateOfBirth, String address){
-        try{
-            LOGGER.info("Entering a trainee");
-            traineeService.createTrainee(firstName, lastName, dateOfBirth, address);
-            LOGGER.info("A trainee was created");
-        } catch (IllegalArgumentException e){
-            LOGGER.error("createTrainee failed for {}", e.getMessage());
-        }
+        LOGGER.info("creating a trainee");
+        traineeService.createTrainee(firstName, lastName, dateOfBirth, address);
+        LOGGER.info("A trainee was created");
     }
 
-    public void updatedTrainee(long userId, LocalDate dateOfBirth, String address) {
-        try {
-            LOGGER.info("Updating a trainee");
-            traineeService.updateTrainee(userId, dateOfBirth, address);
-            LOGGER.info("A trainee was updated");
-        } catch (IllegalArgumentException e){
-            LOGGER.error("updatedTrainee failed for {}", e.getMessage());
-        }
+    public void authenticateTrainee(@NotBlank (message = INVALID_FIELD)
+                                       String username,
+                                       @NotBlank (message = INVALID_FIELD)
+                                       String password){
+        LOGGER.info("Initializing trainee login");
+        authService.authenticateTrainee(username, password);
+    }
+
+    public void authenticateTrainer(@NotBlank (message = INVALID_FIELD)
+                                       String username,
+                                       @NotBlank (message = INVALID_FIELD)
+                                       String password){
+        LOGGER.info("Initializing trainer login");
+        authService.authenticateTrainer(username, password);
+    }
+
+    public Trainee selectTraineeByUsername(@NotBlank (message = INVALID_FIELD)
+                                           String username){
+        LOGGER.info("Selecting a trainee by username");
+        return traineeService.selectTraineeByUsername(username);
+    }
+
+    public Trainer selectTrainerByUsername(@NotBlank (message = INVALID_FIELD)
+                                           String username){
+        LOGGER.info("Selecting a trainer by username");
+        return trainerService.selectTrainerByUsername(username);
+    }
+
+    public void changeTraineePassword(@NotBlank (message = INVALID_FIELD)
+                                      String username,
+                                      @NotBlank (message = INVALID_FIELD)
+                                      String currentPassword,
+                                      @NotBlank (message = INVALID_FIELD)
+                                      String newPassword){
+        LOGGER.info("Initializing Trainee Password Change");
+        traineeService.changePassword(username, currentPassword, newPassword);
+    }
+
+    public void changeTrainerPassword(@NotBlank (message = INVALID_FIELD)
+                                      String username,
+                                      @NotBlank (message = INVALID_FIELD)
+                                      String currentPassword,
+                                      @NotBlank (message = INVALID_FIELD)
+                                      String newPassword){
+        LOGGER.info("Initializing Trainer Password Change");
+        trainerService.changePassword(username, currentPassword, newPassword);
+    }
+
+    public void updatedTrainer(@NotBlank (message = INVALID_FIELD)
+                               String username,
+                               @Positive (message = INVALID_ID_VALUE)
+                               Long specializationId){
+
+
+        trainerService.updateTrainer(username, specializationId);
+        LOGGER.info("A trainer was updated");
+    }
+
+    public void updatedTrainee(@NotBlank (message = INVALID_FIELD)
+                               String username,
+                               @NotNull (message = INVALID_FIELD)
+                               LocalDate dateOfBirth,
+                               @NotNull (message = INVALID_FIELD)
+                               String address) {
+
+        LOGGER.info("Updating a trainee");
+        traineeService.updateTrainee(username, dateOfBirth, address);
+        LOGGER.info("A trainee was updated");
+    }
+
+    public void toggleTraineeActiveStatus(@NotBlank (message = INVALID_FIELD)
+                                          String username){
+        LOGGER.info("Initializing Activate/De-Active a trainee");
+        traineeService.changeActiveStatus(username);
+        LOGGER.info("The status of a trainee changed");
+    }
+
+    public void toggleTrainerActiveStatus(@NotBlank (message = INVALID_FIELD)
+                                          String username) {
+        LOGGER.info("Initializing Activate/De-Active a trainer");
+        trainerService.changeActiveStatus(username);
+        LOGGER.info("The status of a trainer changed");
 
     }
 
-    public void deleteTrainee(long userId){
-        try {
-            LOGGER.info("Deleting a trainee");
-            traineeService.deleteTrainee(userId);
-            LOGGER.info("A trainee was deleted");
-        } catch (IllegalArgumentException e){
-            LOGGER.error("deleteTrainee failed for {}", e.getMessage());
-        }
+    public void deleteTrainee(@NotBlank (message = INVALID_FIELD)
+                              String username){
+        LOGGER.info("Deleting a trainee");
+        traineeService.deleteTrainee(username);
+        LOGGER.info("A trainee was deleted");
     }
 
-    public Trainee selectTrainee(long userId){
-        try {
-            LOGGER.info("Selecting a trainee");
-            return traineeService.selectTrainee(userId);
-        } catch (IllegalArgumentException e) {
-            LOGGER.error("Selecting trainee failed {}",e.getMessage());
-        }
-        return null;
+    public void createTraining (@NotBlank (message = INVALID_FIELD)
+                                String traineeUsername,
+                                @NotBlank (message = INVALID_FIELD)
+                                String trainerUsername,
+                                @NotBlank (message = INVALID_FIELD)
+                                String trainingName,
+                                @Positive (message = INVALID_ID_VALUE)
+                                Long trainingType,
+                                @NotNull
+                                LocalDate trainingDate,
+                                @Min(value = 20, message = INVALID_MIN_VALUE)
+                                @Max(value = 60, message = INVALID_MAX_VALUE)
+                                int durationInMinutes){
+        LOGGER.info("Entering a training");
+        trainingService.createTraining(traineeUsername, trainerUsername, trainingName, trainingType,
+                trainingDate, durationInMinutes);
+        LOGGER.info("A training was created");
     }
 
-    public List<Trainee> selectAllTrainees(){
-        LOGGER.info("Select all trainees");
-        return traineeService.selectAllTrainees();
+    public List<Training> getTraineeTrainings(@NotBlank (message = INVALID_FIELD)
+                                              String username, LocalDate from, LocalDate to,
+                                              String trainerName, Long trainingTypeId){
+        LOGGER.info("Getting trainee training list");
+        return traineeService.getTraineeTrainings( username, from, to, trainerName, trainingTypeId);
     }
 
-    public void createTrainer(String firstName, String lastName, String specialization){
-        try {
-            LOGGER.info("Entering a trainer");
-            trainerService.createTrainer(firstName,lastName,specialization);
-            LOGGER.info("A trainer was created");
-        } catch (IllegalArgumentException e) {
-            LOGGER.error("createTrainer failed for {}", e.getMessage());
-        }
+    public List<Training> getTrainerTrainings(@NotBlank (message = INVALID_FIELD)
+                                              String username, LocalDate from, LocalDate to,
+                                              String traineeName){
+        LOGGER.info("Getting trainer training list");
+        return trainerService.getTrainerTrainings( username, from, to, traineeName);
     }
 
-    public void updatedTrainer(long userId, String specialization){
+    public List<Trainer> getUnassignedTrainersByTraineeUsername(@NotBlank (message = INVALID_FIELD)
+                                                                String traineeUsername){
+        LOGGER.info("Getting a Unassigned Trainers by Trainee Username");
+        return trainerService.getUnassignedTrainersByTraineeUsername(traineeUsername);
 
-        try {
-            LOGGER.info("Updating a trainer");
-            trainerService.updateTrainer(userId,specialization);
-            LOGGER.info("A trainer was updated");
-        } catch (IllegalArgumentException e){
-            LOGGER.error("updatedTrainer failed for {}", e.getMessage());
-        }
     }
 
-    public Trainer selectTrainer(long userId){
-
-        try {
-            LOGGER.info("Selecting a trainer");
-            return trainerService.selectTrainer(userId);
-        } catch (IllegalArgumentException e) {
-            LOGGER.error("Selecting trainer failed {}",e.getMessage());
-        }
-        return null;
-    }
-
-    public List<Trainer> selectAllTrainers(){
-        LOGGER.info("Select all trainers");
-        return trainerService.selectAllTrainers();
-    }
-
-    public void createTraining (long traineeId, long trainerId, String trainingName,
-                                String type, LocalDate trainingDate, int durationInMinutes){
-        try {
-            LOGGER.info("Entering a training");
-            trainingService.createTraining(traineeId, trainerId, trainingName, type,
-                    trainingDate, durationInMinutes);
-            LOGGER.info("A training was created");
-        } catch (IllegalArgumentException e) {
-            LOGGER.error("createTraining failed for {}", e.getMessage());
-        }
-    }
-
-    public Training selectTraining(long trainingId){
-        try {
-            LOGGER.info("Selecting a training");
-            return trainingService.selectTraining(trainingId);
-        } catch (IllegalArgumentException e) {
-            LOGGER.error("Selecting training failed {}",e.getMessage());
-        }
-        return null;
-    }
-
-    public List<Training> selectAllTrainings(){
-        LOGGER.info("Select all trainings");
-        return trainingService.selectAllTrainings();
-    }
 
 }
