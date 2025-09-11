c6d21cbb86c660bfebe2cdcca810d3b206e71a62
Jesús Monroy <jdmonroyg@unal.edu.co>
2025-09-11 18:17:59 -0500

    refactor(model): Changing some field names in the model, a line was added in the openapi configuration and constants

--- src/main/java/com/epam/config/OpenApiConfig.java
+++ src/main/java/com/epam/config/OpenApiConfig.java
@@ -7,8 +7,11 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 /**
  * @author jdmon on 6/09/2025
@@ -21,6 +24,7 @@ public class OpenApiConfig {
     public OpenAPI customOpenApi(){
         final String securitySchemeName = "Authorization";
         return new OpenAPI()
+                .servers(List.of(new Server().url("/api/v1")))
                 .info(new Info()
                         .title("GymCRM API")
                         .version("1.0")
